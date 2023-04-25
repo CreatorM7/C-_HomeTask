4a8dc726896649f8e2752be013a1fd399188e424
CreatorM7 <43927621+CreatorM7@users.noreply.github.com>
2023-04-25 11:45:38 +0300

    Add files via upload

---
+++ СSharp_Hometask_3_Gen/СSharp_Hometask_3_Gen/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public interface IStartable
+{
+    void Start();
+}
+
+public interface IClouseble
+{
+    void Close();
+}
+public class Factory<T> where T : class, new()
+{
+    public T Create()
+    {
+        return new T();
+    }
+}
+public class Util : IStartable, IClouseble
+{
+    public void Start()
+    {
+        Console.WriteLine("Factory Begen work");
+    }
+
+    public void Close()
+    {
+        Console.WriteLine("Factory stop proces");
+    }
+}
+public static class ExtensionMethods
+{
+    public static void DaylyProces<T>(this T item) where T : class, IStartable, IClouseble
+    {
+        item.Start();
+        item.Close();
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Factory<Util> factory = new Factory<Util>();
+        Util workDay = factory.Create();
+        workDay.DaylyProces();
+    }
+}
